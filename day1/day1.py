3ba2220ac0ab65c6cfc4ea842ac62532d3821521
HicaroD <hdanrlley1@gmail.com>
2022-12-02 15:31:35 -0300

    OAC: solution to the second part of day 1 problem

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,22 @@
-def main():
+def get_top_three_elfis(calories: list[int]) -> list[int]:
+    top_three_elfis = []
+
+    # TODO: this code is too repetitive
+    greatest_calorie = max(calories)
+    top_three_elfis.append(greatest_calorie)
+    calories.remove(greatest_calorie)
+
+    greatest_calorie = max(calories)
+    top_three_elfis.append(greatest_calorie)
+    calories.remove(greatest_calorie)
+
+    greatest_calorie = max(calories)
+    top_three_elfis.append(greatest_calorie)
+    calories.remove(greatest_calorie)
+
+    return top_three_elfis
+
+def get_elf_calories() -> list[int]:
     calories = []
 
     with open("input.txt") as file:
@@ -11,8 +29,12 @@ def main():
                 calories.append(calorie_sum)
                 calorie_sum = 0
 
+    return calories
 
-    print(max(calories))
+def main():
+    calories = get_elf_calories()
+    print(max(calories)) # Solution to the first part of the problem
+    print(sum(get_top_three_elfis(calories))) # Solution to the second part of the problem
 
 if __name__ == "__main__":
     main()
